429e31cfa6a499b66d61ba0234b16817524ec6bc
Volin13 <110535249+Volin13@users.noreply.github.com>
2022-11-10 00:52:13 +0200

    asda

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,31 +1,33 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-
-const localStorageData = "videoplayer-current-time"
+const localStorageData = 'videoplayer-current-time';
 
 const iframe = document.querySelector('iframe');
-    const player = new Player(iframe);
-    
-    let currentTime = 0
-    player.on('timeupdate', throttle(onCurrentTime, 1000))
+const player = new Player(iframe);
 
-    function onCurrentTime(currentTime){
-        const stringifyedtime = JSON.stringify(currentTime);
-        localStorage.setItem(localStorageData, stringifyedtime);
+let currentTime = 0;
+player.on('timeupdate', throttle(onCurrentTime, 1000));
+
+function onCurrentTime(currentTime) {
+  const stringifyedtime = JSON.stringify(currentTime);
+  localStorage.setItem(localStorageData, stringifyedtime);
+}
+// player.off('timeupdate');
+const reloadTime = JSON.parse(localStorage.getItem(localStorageData));
+player
+  .setCurrentTime(reloadTime.seconds)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
     }
-    // player.off('timeupdate');
-    const reloadTime = JSON.parse(localStorage.getItem(localStorageData))
-    player.setCurrentTime(reloadTime.seconds).then(function(seconds) {
-        // seconds = the actual time that the player seeked to
-    }).catch(function(error) {
-        switch (error.name) {
-            case 'RangeError':
-                // the time was less than 0 or greater than the video’s duration
-                break;
-    
-            default:
-                // some other error occurred
-                break;
-        }
-    });
+  });
